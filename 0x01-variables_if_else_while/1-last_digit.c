3984a10197f105c77b7a344a7c971befb1fa0d0c
Gabbylim <gabrianelim@gmail.com>
2022-07-02 14:11:29 +0300

    random var

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'simple number checker'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d", n);
+if (n > 5)
+{
+printf(" is %d and is greater than 5\n", (rand() - RAND_MAX / 2));
+}
+else if (n == 0)
+{
+printf(" is %d and is 0\n", (rand() - RAND_MAX / 2));
+}
+else if (n < 6 && n != 0)
+{
+printf(" is %d and is less than 6 and not 0\n", rand() - RAND_MAX / 2);
+}
+else
+{
+printf("repeat");
+}
+return (0); }
