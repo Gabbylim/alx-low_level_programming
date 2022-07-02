d2242470c0eccde568b29ac837c51e4df5c70127
Gabbylim <gabrianelim@gmail.com>
2022-07-02 14:15:44 +0300

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ printf(" is %d and is greater than 5\n", (rand() - RAND_MAX / 2));
 }
 else if (n == 0)
 {
-printf(" is %d and is 0\n", (rand() - RAND_MAX / 2));
+printf(" is %d and is 0\n", n);
 }
 else if (n < 6 && n != 0)
 {
-printf(" is %d and is less than 6 and not 0\n", rand() - RAND_MAX / 2);
+printf(" is %d and is less than 6 and not 0\n", n);
 }
 else
 {
