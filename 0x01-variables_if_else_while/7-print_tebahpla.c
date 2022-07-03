13bc5d7e53d7730a4678697515e3f1e5afa77847
Gabbylim <gabrianelim@gmail.com>
2022-07-03 17:00:35 +0300

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int x;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0); }
