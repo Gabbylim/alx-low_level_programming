96e1ccee820e6766bb0991d0ea1652c4c2578065
Gabbylim <gabrianelim@gmail.com>
2022-07-04 15:38:36 +0300

    combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the advances'
+ *
+ * Return: Always a zero
+ */
+int main(void)
+{
+int x;
+int y;
+
+for(x=0;x<9;x++)
+{
+for(y=0;y<10;y++)
+{
+if(x != y && x !=8 && y !=9)
+{
+putchar(x + '0');
+putchar(y + '0');
+putchar(',');
+putchar(' ');
+}
+
+}
+}
+
+
+}
