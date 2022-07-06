eae8e951bc7521af14d9355b53a6f2b5f429c00e
Gabbylim <gabrianelim@gmail.com>
2022-07-06 11:45:06 +0300

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,13 @@
  * Description: works like a putchar but from a different library
  *
  * Return: Always zero when in success
- *
-
  */
 int main(void)
 {
-char str[9]= "_putchar";
+char str[9] = "_putchar";
 int x;
 
-for(x=0;x<9;x++)
+for (x = 0; x < 9 ; x++)
 {
 _putchar(str[x]);
 }
