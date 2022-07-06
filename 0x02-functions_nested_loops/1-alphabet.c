4ef26500ab08b901317ac4e01078253367da30f8
Gabbylim <gabrianelim@gmail.com>
2022-07-06 15:07:27 +0300

    alphabet printer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print alphabet function
+ *
+ * Description: 'alphabet print'
+ *
+ * Return: 'none'
+ */
+
+void print_alphabet(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
