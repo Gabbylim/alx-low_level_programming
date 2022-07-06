1601ca261e26a091d72df65ee9687daec00e1c98
Gabbylim <gabrianelim@gmail.com>
2022-07-06 15:47:32 +0300

    x10 print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - the printer
+ *
+ * Description: 'prints 10x'
+ *
+ * Return: 'always void'
+ * On error it returns -1.
+ */
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+int j;
+while (i < 10)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n')
+i++;
+}
+}
