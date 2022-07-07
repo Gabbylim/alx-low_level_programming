d47751fd430068ec60bc37d6621e18db77f017c2
Gabbylim <gabrianelim@gmail.com>
2022-07-07 16:09:46 +0300

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks for alphabets
+ * @c: this is an int type var
+ * Return: either 0 or 1
+ */
+
+int _isupper(int c)
+{
+
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}}
