57fe9a6fe7d22c222dd8ee7f856057c4dff09e1f
Gabbylim <gabrianelim@gmail.com>
2022-07-11 15:39:13 +0300

    report by its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+
+/**
+ * _strlen - returns the length of a string
+ * @s: its a pointer to a string
+ * Return: value of string length
+ */
+
+int _strlen(char *s)
+{
+long int n;
+
+for (n = 0; s[n] != '\0'; n++)
+{
+;
+}
+return (n);
+}
