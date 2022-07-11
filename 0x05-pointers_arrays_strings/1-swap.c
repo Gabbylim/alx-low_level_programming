b66b6994067901d3da40085bf12a608d3c5b89d1
Gabbylim <gabrianelim@gmail.com>
2022-07-11 14:38:33 +0300

    Dont swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+
+/**
+ * swap_int - swaps values of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+
+}
