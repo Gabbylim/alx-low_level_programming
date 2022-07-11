107776446e39ed0cf2efbb3312d3521806c5ba7e
Gabbylim <gabrianelim@gmail.com>
2022-07-11 12:22:10 +0300

    Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+
+/**
+ * reset_to_98 - updates the value it points to using the pointer
+ *
+ * @n: this is the pointer
+ * Return: returns 0
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This these are pointer tasks
