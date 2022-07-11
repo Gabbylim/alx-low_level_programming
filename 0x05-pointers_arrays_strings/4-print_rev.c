31de61a158fc4e3fc117f7393920bcf0fe1bd908
Gabbylim <gabrianelim@gmail.com>
2022-07-11 16:50:08 +0300

    i can go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse a string
+ * @s: char array string type
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+;
+}
+
+for (i--; i >= 0; i--)
+{
+_putchar(s[i]);
+
+
+}
+_putchar('\n');
+}
+
