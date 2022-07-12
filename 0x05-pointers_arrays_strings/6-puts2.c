5879a64b88e5b4c84348deaa5bec610137d12500
Gabbylim <gabrianelim@gmail.com>
2022-07-12 11:15:42 +0300

    half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints any other number in the array
+ * @str: pointer to the array
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+int x;
+
+for (x = 0; str[x] != '\0'; x++)
+{
+if (str[x] % 2 == 0)
+{
+_putchar(str[x]);
+}
+}
+}
