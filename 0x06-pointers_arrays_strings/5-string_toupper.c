ecaf6b941a80995ad9334b61f21cfe1a78d678cf
Gabbylim <gabrianelim@gmail.com>
2022-07-14 11:30:31 +0300

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper - changes to upper
+ * @c: - string
+ * Return: string
+ */
+char *string_toupper(char *c)
+{
+int i;
+for (i = 0; c[i] != '\0'; i++)
+{
+if (c[i] > 96 && c[i] < 123)
+{
+c[i] -= 32;
+}
+}
+return (c); }
