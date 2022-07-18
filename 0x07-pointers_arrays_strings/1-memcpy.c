c015426afb3243e699ed161eda18fff149f48100
Gabbylim <gabrianelim@gmail.com>
2022-07-18 15:15:53 +0300

    mecpy correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,14 +2,15 @@
  * _memcpy - copies nbytes from src to dest
  * @dest: area to copy to
  * @src : area to copy from
+ * @n: mem size
  * Return: memory copied
  */
 
-char *_memcpy(char *dest,char *src,unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 int i;
 i = 0;
-while (n > 0 )
+while (n > 0)
 {
 dest[i] = src[i];
 i++;
